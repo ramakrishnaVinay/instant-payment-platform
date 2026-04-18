5d7ec7c6087e651e890fc7592de7018f78dc7c01
Your Name <your-email@example.com>
2026-04-18 13:29:13 +0530

    payment processor

---
+++ payment-procesor/src/main/java/com/payment/procesor/PaymentProcesorApplication.java
@@ -0,0 +1,13 @@
+package com.payment.procesor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcesorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcesorApplication.class, args);
+	}
+
+}
